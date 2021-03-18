f008e25981f15835d9b8860f3867954b654bc40f
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-18 16:20:13 +0100

    Aggiunta possibilità per l'utente di inserire articoli nella lista della spesa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
 $(document).ready(function() {
 
-  // Inserisci il codice qui
+  // var listaSpesa = [
+  //   "Latte",
+  //   "Cereali",
+  //   "Birra",
+  //   "Pane",
+  // ];
+
+
+  var listaSpesa = [];
+
+  // Al click del bottone l'utente può inserire articoli nella lista della spesa
+  $("button").click(
+    function() {
+
+      var num = parseInt(prompt("Quanti articoli vuoi inserire?"));
+
+      for (var i = 1; i <= num; i++) {
+        var articolo = prompt("Aggiungi il " + i + "° articolo:");
+        listaSpesa.push(articolo);
+      }
+
+      for (var i = 0; i < listaSpesa.length; i++) {
+
+        var template = $('.template li').clone();
+        template.prepend(listaSpesa[i]);
+        $('.tobuy-list').append(template);
+
+      }
+
+      listaSpesa = [];
+
+    }
+  );
+
+
+
+  // Al click della X l'utente cancella l'articolo selezionato
+  $("span").click(
+    function() {
+      $(this).parent().remove();
+    }
+  );
 
 });

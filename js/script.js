22519c6ff3bcad31f64f829573adce6f8bc88aff
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-18 19:24:33 +0100

    Eliminati button e alerts e aggiunto input e keydown

--- js/script.js
+++ js/script.js
@@ -1,27 +1,49 @@
 $(document).ready(function() {
 
   // Al click del bottone l'utente può inserire articoli nella lista della spesa
-  $("button").click(
-    function() {
+  // $("button").click(
+  //   function() {
+  //
+  //     var listaSpesa = [];
+  //     var num = parseInt(prompt("Quanti articoli vuoi inserire?"));
+  //
+  //     for (var i = 1; i <= num; i++) {
+  //       var articolo = prompt("Aggiungi il " + i + "° articolo:");
+  //       listaSpesa.push(articolo);
+  //     }
+  //
+  //     for (var j = 0; j < listaSpesa.length; j++) {
+  //
+  //       var template = $('.template li').clone();
+  //       template.prepend(listaSpesa[j]);
+  //       $('.tobuy-list').append(template);
+  //
+  //     }
+  //   }
+  // );
 
-      var listaSpesa = [];
-      var num = parseInt(prompt("Quanti articoli vuoi inserire?"));
 
-      for (var i = 1; i <= num; i++) {
-        var articolo = prompt("Aggiungi il " + i + "° articolo:");
-        listaSpesa.push(articolo);
-      }
 
-      for (var j = 0; j < listaSpesa.length; j++) {
 
+  $('input').keydown(
+    function(evento) {
+      
+      if ((evento.which) == 13) {
+
+        var articolo = $('input').val();
         var template = $('.template li').clone();
-        template.prepend(listaSpesa[j]);
-        $('.tobuy-list').append(template);
 
+        if (articolo.trim() != '') {
+
+          template.prepend(articolo);
+          $('input').val('');
+          $('.tobuy-list').append(template);
+
+        }
       }
-
     }
   );
+
 
   // Al click della X l'utente cancella l'articolo selezionato
   $(document).on('click', 'span', function() {
